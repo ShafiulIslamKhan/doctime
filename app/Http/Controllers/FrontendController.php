9930da1bf7773553a87c54397c33771b0a13e175
admin <@gmail.com>
2022-07-17 12:07:55 +0600

    Doctor and patient pages uploads

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -13,4 +13,60 @@ class FrontendController extends Controller
     {
         return view('frontend.home');
     }
+
+    /**
+     * Show login page
+     */
+    public function showLoginPage()
+    {
+        return view('frontend.login');
+    }
+
+    /**
+     * Show patient reg page
+     */
+    public function showPatientRegisterPage()
+    {
+        return view('frontend.patient.register');
+    }
+
+    /**
+     * Show patient dash page
+     */
+    public function showPatientDashPage()
+    {
+        return view('frontend.patient.dashboard');
+    }
+
+
+
+    /**
+     * Show doctor reg page
+     */
+    public function showDoctorRegisterPage()
+    {
+        return view('frontend.doctor.register');
+    }
+
+    /**
+     * Show doctor dash page
+     */
+    public function showDoctorDashPage()
+    {
+        return view('frontend.doctor.dashboard');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
