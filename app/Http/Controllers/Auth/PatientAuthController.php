495763e7ad97df6adb423aedd76468b5c104eb30
admin <@gmail.com>
2022-07-26 20:32:24 +0600

    Patient auth sys done

---
+++ app/Http/Controllers/Auth/PatientAuthController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\Patient;
+use Illuminate\Http\Request;
+use Symfony\Contracts\Service\Attribute\Required;
+
+class PatientAuthController extends Controller
+{
+    
+    /**
+     * Patient register 
+     */
+    public function register(Request $request)
+    {
+        //Data validate
+        $this -> validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'cell' => 'required',
+            'pass' => 'required|confirmed'
+        ]);
+
+        //data store
+        $patient = Patient::create([
+            'name' => $request -> name,
+            'email' => $request -> email,
+            'mobile' => $request -> cell,
+            'password' => password_hash( $request -> pass, PASSWORD_DEFAULT )
+        ]);
+
+        //Return back
+        return redirect() -> route('patient.reg.page') -> with( 'success', " Hi " . $patient -> name ." , your account is ready. Now login. " );
+
+
+        
+
+    }
+
+
+    /**
+     * Patient login 
+     */
+    public function login(Request $request)
+    {
+        //Data validate
+        $this -> validate($request, [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        return $request -> all();
+
+
+        
+
+    }
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\PatientAuthController;
 use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,14 +11,17 @@ Route::get('/login', [ FrontendController::class, 'showLoginPage' ]) -> name('lo
 
 // Patient pages
 Route::get('/patient-register', [ FrontendController::class, 'showPatientRegisterPage' ]) -> name('patient.reg.page');    
-Route::get('/patient-dashboard', [ FrontendController::class, 'showPatientDashPage' ]) -> name('patient.dash.page');    
+Route::get('/patient-dashboard', [ FrontendController::class, 'showPatientDashPage' ]) -> name('patient.dash.page');  
+Route::post('/patient-register', [ PatientAuthController::class, 'register' ]) -> name('patient.register');
+Route::post('/patient-login', [ PatientAuthController::class, 'login' ]) -> name('patient.login');
+
 
 // Doctor pages
 Route::get('/doctor-register', [ FrontendController::class, 'showDoctorRegisterPage' ]) -> name('doctor.reg.page');    
 Route::get('/doctor-dashboard', [ FrontendController::class, 'showDoctorDashPage' ]) -> name('doctor.dash.page'); 
 
 
-//Route declaration for laravel
+
 
 
 
